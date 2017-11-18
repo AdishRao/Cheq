560363e7d50c3bbd3aef8d0efc7a786b241b2961
Aniruddha Mysore <aniruddha.mysore@gmail.com>
2017-11-18 13:54:26 +0530

    Firebase Storage and SBI API Upload

--- app/src/main/java/lw/cheq/Upload.java
+++ app/src/main/java/lw/cheq/Upload.java
@@ -1,37 +1,55 @@
 package lw.cheq;
 
+import android.app.ProgressDialog;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.google.android.gms.common.api.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Upload extends AppCompatActivity implements View.OnClickListener {
     Bitmap bmp = null;
     ImageView cheque;
-    private Button login;
     HashMap<String, EditText> fields = new HashMap<>();
-    ApplicationController applicationController = new ApplicationController();
+    CheckBox sbiapi;
+    View view;
+    ProgressDialog progressDialog;
+    FirebaseStorage storage;
+    // Create a storage reference from our app
+    StorageReference storageRef;
+    private Button upload;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
-
+        view = findViewById(R.id.sample_main_layout);
         String filename = getIntent().getStringExtra("image");
         try {
             FileInputStream is = this.openFileInput(filename);
@@ -44,9 +62,14 @@ public class Upload extends AppCompatActivity implements View.OnClickListener {
         cheque = findViewById(R.id.cheqimage);
         cheque.setImageBitmap(bmp);
 
-        login = findViewById(R.id.button);
+        upload = findViewById(R.id.button);
 
-        login.setOnClickListener(this);
+        upload.setOnClickListener(this);
+
+        sbiapi = findViewById(R.id.SBIAPI);
+
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
 
         fields.put("@string/chq_num", (EditText) findViewById(R.id.CHQ_NUM));
         fields.put("@string/amount_words", (EditText) findViewById(R.id.AMOUNT_WORDS));
@@ -62,45 +85,125 @@ public class Upload extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
-        if (view == login) {
-            sendToAPI();
+        if (view == upload) {
+            // TODO @Adish do form validation here, before calling these functions
+
+            sendToFirebase();
+            if (sbiapi.isChecked()) {
+                sendToAPI();
+            }
         }
+    }
+
+    private void sendToFirebase() {
+        // Get cheque number from field
+        String chq_num = fields.get("CHQ_NUM").getText().toString();
+        // We create a StorageReference to <cheque_number.jpg>
+        final StorageReference chequeRef = storageRef.child(chq_num + ".jpg");
+
+        // Detect if file already exists
+        chequeRef.getDownloadUrl().addOnFailureListener(new OnFailureListener() {
+            // if file does not exist do this
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                // Convert bmp imag to byte array
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                byte[] data = baos.toByteArray();
+
+                // Upload
+                UploadTask uploadTask = chequeRef.putBytes(data);
+                // Upload listeners
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        Snackbar snackbar = Snackbar
+                                .make(view, "Error Uploading to Firebase : " + exception.getMessage(), Snackbar.LENGTH_LONG);
+
+                        snackbar.show();
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                        Snackbar snackbar = Snackbar
+                                .make(view, "Pushed Successfully!", Snackbar.LENGTH_LONG);
+
+                        snackbar.show();
+                    }
+                });
+                StorageMetadata metadata = new StorageMetadata.Builder()
+                        .setContentType("image/jpg")
+                        .setCustomMetadata("CHQ_NUM", fields.get("CHQ_NUM").getText().toString())
+                        .build();
+            }
+
+        });
     }
 
     private void sendToAPI() {
         final String URL = "https://private-anon-f1ac857084-chequeinsertrecord.apiary-mock.com/InsertChqDetails/";
+
         // Post params to be sent to the server
         HashMap<String, String> params = new HashMap<>();
-        params.put("token", "AbCdEfGh123456");
 
-        JsonObjectRequest req = new JsonObjectRequest(URL, new JSONObject(params),
-                new com.android.volley.Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            VolleyLog.v("Response:%n %s", response.toString(4));
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                },
-                new com.android.volley.Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        VolleyLog.e("Error: ", error.getMessage());
-                    }
+        progressDialog = new ProgressDialog(Upload.this);
+        progressDialog.setMessage("Uploading, please wait...");
+        progressDialog.show();
 
-                });
+        //converting image to base64 string
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        final String imageString = Base64.encodeToString(imageBytes, Base64.DEFAULT);
 
-        // add the request object to the queue to be executed
-        try {
-            applicationController.addToRequestQueue(req);
-        } catch (NullPointerException ex) {
-            ex.printStackTrace();
-        }
+        //sending image to server
+        StringRequest request = new StringRequest(Request.Method.POST, URL, new com.android.volley.Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                progressDialog.dismiss();
+                if (s.equals("true")) {
+                    Toast.makeText(Upload.this, "Uploaded Successful", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(Upload.this, "Some error occurred!", Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new com.android.volley.Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                Toast.makeText(Upload.this, "Some error occurred -> " + volleyError, Toast.LENGTH_LONG).show();
+            }
+        }) {
+            //adding parameters to send
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parameters = new HashMap<>();
+                parameters.put("image", imageString);
+                return parameters;
+            }
 
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
 
-        login.setVisibility(View.INVISIBLE);
+                HashMap<String, String> headers = new HashMap<>();
 
+                for (Map.Entry<String, EditText> entry : fields.entrySet()) {
+                    headers.put(entry.getKey(), entry.getValue().getText().toString());
+                }
+                headers.put("api-key", getString(R.string.api_key));
+                headers.put("TEAM_ID", getString(R.string.team_id));
+                headers.put("MIME_TYPE", "image/jpeg");
+                headers.put("ENCODING", "Base64");
+                // TODO Find out a way to calculate image size and put it over here
+                //headers.put("IMG_SIZE", )
+                return headers;
+            }
+
+        };
+
+        RequestQueue rQueue = Volley.newRequestQueue(Upload.this);
+        rQueue.add(request);
+
+        //upload.setVisibility(View.INVISIBLE);
     }
 }
